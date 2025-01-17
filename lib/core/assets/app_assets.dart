c329d5a533d0927ae72203292ea67ca87231919f
Myproject <mohamedAlaa-CS>
2025-01-17 19:46:58 +0200

    + add splash view widget

---
+++ lib/core/assets/app_assets.dart
@@ -0,0 +1,4 @@
+abstract class AppAssets {
+  static const String splash_1 = 'assets/images/splash_screen_1.png';
+  static const String splash_2 = 'assets/images/splash_screen_2.png';
+}
---
+++ lib/core/theme/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  static const Color primaryColor = Color(0XFFE2BE7F);
+  static const Color secondColor = Color(0XFF212121);
+  static const Color white = Color(0XFFFFFFFF);
+}
